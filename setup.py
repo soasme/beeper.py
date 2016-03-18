117c4c33728d8a9e2cd982dbeec6c6bfdb7188ef
soasme <soasme@gmail.com>
2016-03-18 14:50:26 +0800

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='beeper',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/soasme/beeper.py',
     license='MIT',
     author='Ju Lin',

6233da7964f357efb509586aa5ad0440b3bc2039
soasme <soasme@gmail.com>
2016-03-18 14:50:18 +0800

    fix version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='beeper',
-    version='0.1',
+    version='0.1.0',
     url='https://github.com/soasme/beeper.py',
     license='MIT',
     author='Ju Lin',

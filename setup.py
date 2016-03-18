2233bf8cf27628d66ac2678aeda787a45bb66aa4
Soasme <soasme@users.noreply.github.com>
2016-03-18 14:38:10 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+Bundle virtualenv and project to an artifact.
+"""
+
+from setuptools import setup
+
+setup(
+    name='beeper',
+    version='0.1',
+    url='https://github.com/soasme/beeper',
+    license='MIT',
+    author='Ju Lin',
+    author_email='soasme@gmail.com',
+    description='Bundle virtualenv and project to an artifact.',
+    long_description=__doc__,
+    packages=['beeper'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'click>=1.0',
+        'PyYAML>=3.11',
+    ],
+    entry_points={
+        'console_scripts': [
+            'beeper = beeper:build'
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)

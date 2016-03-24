0a4f71a04a9f20388dbd8522f309999123c7b45e
soasme <soasme@gmail.com>
2016-03-24 12:36:08 +0800

    fix manifest set.

--- beeper.py
+++ beeper.py
@@ -139,8 +139,8 @@ def build(version, conf):
     conf.setdefault('postinstall', [])
     conf.setdefault('manifest', set())
     conf.setdefault('current_dir', run('pwd'))
-    conf.setdefault('version', '')
-    conf.setdefault('manifest', set(conf['manifest']))
+    conf['version'] = version
+    conf['manifest'] = set(conf['manifest'])
 
     run('rm -rf venv/ dist/')
     run('mkdir -p dist/')

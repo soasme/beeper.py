422b06bc7c3e1a7b0447c6098c7ada1244ad2064
soasme <soasme@gmail.com>
2016-03-25 17:32:23 +0800

    amending test.

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -19,34 +19,35 @@ def teardown_function(f):
         run('rm -rf dist/')
 
 def test_compress():
-    run('python beeper/cmd.py build --version 1')
+    run('python beeper/cmd.py build --version 1 --conf tests/beeper.yml')
     assert os.path.exists('dist/test-1.tgz')
 
 def test_no_compress():
-    run('python beeper/cmd.py build --version 1 --no-compress')
+    run('python beeper/cmd.py build --version 1 --no-compress --conf tests/beeper.yml')
     assert os.path.exists('dist/test-1.tar')
 
 def test_version_default_is_none():
-    run('python beeper/cmd.py build --no-compress')
+    run('python beeper/cmd.py build --no-compress --conf tests/beeper.yml')
     assert os.path.exists('dist/test-none.tar')
 
 def test_file_included_in_manifest():
-    run('python beeper/cmd.py build --version 1')
+    run('python beeper/cmd.py build --version 1 --conf tests/beeper.yml')
     run('cd dist; tar xvzf test-1.tgz')
+    run('ls dist')
     for filename in (
             'dist/.beeper-data/virtualenv.py',
             'dist/.beeper-data/requirements.txt',
-            'dist/hello.py',
+            'dist/tests/hello.py',
             'dist/install.sh',
         ):
         assert os.path.exists(filename)
 
 def test_install_venv():
-    run('python beeper/cmd.py build --version 1')
+    run('python beeper/cmd.py build --version 1 --conf tests/beeper.yml')
     run('cd dist; tar xvzf test-1.tgz')
     run('cd dist; ./install.sh')
     assert os.path.exists('dist/venv/bin/python')
     assert os.path.exists('dist/venv/bin/pip')
     assert os.path.exists('dist/venv/bin/py.test')
-    with open('dist/MESSAGE') as f:
+    with open('.beeper-data/MESSAGE') as f:
         assert f.read() == 'BEEPER\n'

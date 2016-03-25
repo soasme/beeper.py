1cd3312fd124c5c2a561ceba7c12fd4dd7f96798
soasme <soasme@gmail.com>
2016-03-25 15:18:56 +0800

    remove useless code.

--- beeper/cmd.py
+++ beeper/cmd.py
@@ -8,19 +8,6 @@ import sys
 import click
 
 
-class cd(object):
-    """Context manager for changing the current working directory"""
-    def __init__(self, newPath):
-        self.newPath = os.path.expanduser(newPath)
-
-    def __enter__(self):
-        self.savedPath = os.getcwd()
-        os.chdir(self.newPath)
-
-    def __exit__(self, etype, value, traceback):
-        os.chdir(self.savedPath)
-
-
 def parse_yaml(file):
     import yaml
     with open(file, 'rb') as f:

b8d7d152b0e779e6224e82d2103668f72e305f49
soasme <soasme@gmail.com>
2016-04-15 10:11:13 +0800

    add `stat` command.`

--- beeper/cmd.py
+++ beeper/cmd.py
@@ -17,6 +17,7 @@ from .builders.core import (
         run_postbuild,
         dist_manifest,
         make_target,
+        get_target_filename,
 )
 from .utils import parse_yaml, run
 
@@ -28,11 +29,7 @@ def main():
 def version():
     click.secho(__version__, fg='green')
 
-@main.command()
-@click.option('--version', default='none')
-@click.option('--format', default='tgz', type=click.Choice(['tar', 'tgz', 'tar.gz', 'zip']))
-@click.option('--conf', default='./beeper.yml')
-def build(version, format, conf):
+def _read_conf(conf, version, format):
     try:
         conf = parse_yaml(conf)
     except:
@@ -48,20 +45,38 @@ def build(version, format, conf):
     conf.setdefault('postinstall', [])
     conf.setdefault('postinstall_commands', '\n'.join(conf.get('postinstall')))
     conf.setdefault('manifest', set())
-    conf.setdefault('current_dir', run('pwd'))
+    conf.setdefault('current_dir', os.environ.get('WORK_DIR') or os.getcwd())
     conf.setdefault('scripts', [])
     conf['postbuild'] = conf['scripts']
     conf['version'] = version
     conf['manifest'] = set(conf['manifest'])
     conf['format'] = format
+    return conf
 
+@main.command()
+@click.option('--version', default='none')
+@click.option('--format', default='tgz', type=click.Choice(['tar', 'tgz', 'tar.gz', 'zip']))
+@click.option('--conf', default='./beeper.yml')
+def build(version, format, conf):
+    conf = _read_conf(conf, version, format)
     with within_build_dir():
-        set_env(conf)
+        set_env()
         prepare_dist(conf)
         build_for_language(conf)
         run_postbuild(conf)
         dist_manifest(conf)
         make_target(conf)
+
+@main.command(context_settings=dict(
+    ignore_unknown_options=True,
+))
+@click.option('--version', default='none')
+@click.option('--format', default='tgz', type=click.Choice(['tar', 'tgz', 'tar.gz', 'zip']))
+@click.option('--conf', default='./beeper.yml')
+@click.argument('ls_args', nargs=-1, type=click.UNPROCESSED)
+def stat(version, format, conf, ls_args):
+    conf = _read_conf(conf, version, format)
+    run('cd %s; ls %s %s' % ('dist', ' '.join(ls_args), get_target_filename(conf)))
 
 if __name__ == '__main__':
     main()

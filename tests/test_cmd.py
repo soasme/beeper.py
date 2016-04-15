10a17a6637432af2a530342b721086239b2b50ef
soasme <soasme@gmail.com>
2016-04-15 10:11:32 +0800

    test stat.

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -18,27 +18,33 @@ def teardown_function(f):
     if os.path.exists('dist/'):
         run('rm -rf dist/')
 
-def run_beeper(options):
-    run('beeper build %s --conf %s' % (options, os.path.join(os.getcwd(), 'tests/beeper.yml')))
+def run_beeper(command='build', options='', **kwargs):
+    return run('beeper %s %s --conf %s' % (
+        command, options, os.path.join(os.getcwd(), 'tests/beeper.yml')), **kwargs)
 
 def test_make_tgz():
-    run_beeper('--version 1')
+    run_beeper('build', '--version 1')
     assert os.path.exists('dist/test-1.tgz')
 
+def test_stat():
+    run_beeper('build', '--version 1')
+    output = run_beeper('stat', '--version 1', capture=True)
+    assert output.splitlines()[-1] == 'test-1.tgz'
+
 def test_make_tar():
-    run_beeper('--version 1 --format tar')
+    run_beeper('build', '--version 1 --format tar')
     assert os.path.exists('dist/test-1.tar')
 
 def test_make_zip():
-    run_beeper('--version 1 --format zip')
+    run_beeper('build', '--version 1 --format zip')
     assert os.path.exists('dist/test-1.zip')
 
 def test_version_default_is_none():
-    run_beeper('')
+    run_beeper()
     assert os.path.exists('dist/test-none.tgz')
 
 def test_file_included_in_manifest():
-    run_beeper('--version 1')
+    run_beeper('build', '--version 1')
     run('cd dist; tar xvzf test-1.tgz')
     run('ls dist')
     for filename in (
@@ -50,7 +56,7 @@ def test_file_included_in_manifest():
         assert os.path.exists(filename)
 
 def test_install_venv():
-    run_beeper('--version 1')
+    run_beeper('build', '--version 1')
     run('cd dist; tar xvzf test-1.tgz')
     run('cd dist; ./install.sh')
     assert os.path.exists('dist/venv/bin/python')

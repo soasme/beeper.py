35a84b92bb1f6fc9d8371322fc1c3267dd36ded9
soasme <soasme@gmail.com>
2016-04-15 10:10:42 +0800

    extract get_target_filename.

--- beeper/builders/core.py
+++ beeper/builders/core.py
@@ -30,16 +30,14 @@ def dist_manifest(conf):
         run('cd $WORK_DIR; cp -r %s $BUILD_DIR/' % file)
 
 
-def make_tarball(conf, compress, suffix):
+def make_tarball(conf, compress):
     manifest_files = ' '.join(
         conf['manifest'] | set(['install.sh', '.beeper-data'])
     )
 
-    archive_cmd = 'tar -c{z}f $DIST_DIR/{app}-{ver}.{suffix} {files}'.format(
+    archive_cmd = 'tar -c{z}f $DIST_DIR/{filename} {files}'.format(
         z='z' if compress else '',
-        app=conf['application'],
-        ver=conf['version'],
-        suffix=suffix,
+        filename=get_target_filename(conf),
         files=manifest_files,
     )
 
@@ -49,20 +47,26 @@ def make_zip(conf):
     manifest_files = ' '.join(
         conf['manifest'] | set(['install.sh', '.beeper-data'])
     )
-    archive_cmd = 'zip -r $DIST_DIR/{app}-{ver}.zip {files}'.format(
-        app=conf['application'],
-        ver=conf['version'],
+    archive_cmd = 'zip -r $DIST_DIR/{filename} {files}'.format(
+        filename=get_target_filename(conf),
         files=manifest_files,
     )
     run('cd $BUILD_DIR; ' + archive_cmd)
 
+
+def get_target_filename(conf):
+    return '{app}-{ver}.{suffix}'.format(
+        app=conf['application'],
+        ver=conf['version'],
+        suffix=conf['format']
+    )
+
+
 def make_target(conf):
     if conf['format'] == 'tar':
-        make_tarball(conf, compress=False, suffix='tar')
-    elif conf['format'] == 'tgz':
-        make_tarball(conf, compress=True, suffix='tgz')
-    elif conf['format'] == 'tar.gz':
-        make_tarball(conf, compress=True, suffix='tar.gz')
+        make_tarball(conf, compress=False)
+    elif conf['format'] in {'tar.gz', 'tgz'}:
+        make_tarball(conf, compress=True)
     elif conf['format'] == 'zip':
         make_zip(conf)
     else:
@@ -77,7 +81,7 @@ def within_build_dir():
         run('rm -rf $BUILD_DIR')
         raise exc
 
-def set_env(conf):
+def set_env(conf=None):
     os.environ['WORK_DIR'] = os.getcwd()
     os.environ['DATA_DIR'] = os.path.join(os.environ['BUILD_DIR'], '.beeper-data')
     os.environ['DIST_DIR'] = os.path.join(os.environ['WORK_DIR'], 'dist')
